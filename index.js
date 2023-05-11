a6c848132bd6f3cc62412922a09735f8e2908712
Fifa <ratchanon@techspace.co.th>
2023-05-11 22:45:49 +0700

    add crud api concerts,users

--- index.js
+++ index.js
@@ -14,30 +14,19 @@ app.listen(5000, function () {
 })
 
 
-app.get('/books', function (req, res, next) {
+app.get('/concerts', function (req, res, next) {
   connection.query(
-    'SELECT * FROM `books`',
+    'SELECT * FROM `list_concert`',
     function(err, results, fields) {
       res.json(results);
     }
   );
 })
 
-app.get('/books/:id', function (req, res, next) {
-  const id = req.params.id;
+app.post('/concert/buy', function (req, res, next) {
   connection.query(
-    'SELECT * FROM `books` WHERE `id` = ?',
-    [id],
-    function(err, results) {
-      res.json(results);
-    }
-  );
-})
-
-app.post('/books', function (req, res, next) {
-  connection.query(
-    'INSERT INTO `books`(`book_number`, `book_name`, `status`, `number_of_book`, `image`) VALUES (?, ?, ?, ?, ?)',
-    [req.body.book_number, req.body.book_name, req.body.status, req.body.number_of_book, req.body.image],
+    'INSERT INTO `buy_tickets_trans`(`user_id`, `con_id`, `count_ticket`, `total_price`) VALUES (?, ?, ?, ?)',
+    [req.body.userId, req.body.conId, req.body.amount, req.body.totalPrice],
     function(err, results) {
       res.json(results);
     }
@@ -45,19 +34,29 @@ app.post('/books', function (req, res, next) {
 })
 
 
-app.put('/books', function (req, res, next) {
+app.put('/user/decreas', function (req, res, next) {
   connection.query(
-    'UPDATE `books` SET `book_number`= ?, `book_name`= ?, `status`= ?, `number_of_book`= ?, `image`= ? WHERE id = ?',
-    [req.body.book_number, req.body.book_name, req.body.status, req.body.number_of_book, req.body.image, req.body.id],
+    'UPDATE `users` SET `fname`= ?, `lname`= ?, `username`= ?, `password`= ?, `amount_money`= ?, `avatar`= ? WHERE id = ?',
+    [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.amount_money, req.body.avatar, req.body.id],
     function(err, results) {
       res.json(results);
     }
   );
 })
 
-app.delete('/books', function (req, res, next) {
+app.put('/concert/decreas', function (req, res, next) {
   connection.query(
-    'DELETE FROM `books` WHERE id = ?',
+    'UPDATE `list_concert` SET `name`= ?, `loc`= ?, `show_date`= ?, `amount_of_ticket`= ?, `image`= ?, `price`= ? WHERE id = ?',
+    [req.body.name, req.body.loc, req.body.show_date, req.body.amount_of_ticket, req.body.image, req.body.price, req.body.id],
+    function(err, results) {
+      res.send(results);
+    }
+  );
+})
+
+app.delete('/concert', function (req, res, next) {
+  connection.query(
+    'DELETE FROM `list_concert` WHERE id = ?',
     [req.body.id],
     function(err, results) {
       res.json(results);
@@ -70,6 +69,57 @@ app.get('/users', function (req, res, next) {
     'SELECT * FROM `users`',
     function(err, results, fields) {
       res.json(results);
+    }
+  );
+})
+
+app.get('/users/:id', function (req, res, next) {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM `users` WHERE `id` = ?',
+    [id],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
+// http://localhost:5000/users
+app.post('/users', function(request, response) {
+  // Capture the input fields
+  let username = request.body.username;
+  let password = request.body.password;
+  // Ensure the input fields exists and are not empty
+  if (username && password) {
+    // Execute SQL query that'll select the account from the database based on the specified username and password
+    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+      // If there is an issue with the query, output the error
+      if (error) throw error;
+      // If the account exists
+      if (results.length > 0) {
+        // Authenticate the user
+          //request.session.loggedin = true;
+          //request.session.username = username;
+        // Redirect to home page
+        response.send(results);
+      } else {
+        response.send('Incorrect Username and/or Password!');
+      }     
+      response.end();
+    });
+  } else {
+    response.send('Please enter Username and Password!');
+    response.end();
+  }
+});
+
+app.get('/users/:id/lists', function (req, res, next) {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM `buy_tickets_trans` WHERE `user_id` = ?',
+    [id],
+    function(err, results) {
+      res.send(results);
     }
   );
 })
